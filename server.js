c416c094e6a763f99d908df2966df7f7c9a8bf45
z3itgeist <braga_gabriel@live.com>
2026-03-26 21:32:15 -0300

    bug botões sem som

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const path = require('path');
 app.use(express.json());
 app.use(express.static(__dirname));
-const port = 3000
+const port = process.env.PORT || 3000;
 
 
 const { gerarAudio } = require("./tts");
